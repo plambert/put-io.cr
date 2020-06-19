28e1ff92ccb832a691cdf365429550ba5a7fb09e
Paul M. Lambert <plambert@plambert.net>
2020-06-19 09:00:31 -0700

    add oob authentication

--- src/put-io-cli.cr
+++ src/put-io-cli.cr
@@ -1,15 +1,18 @@
 require "json"
 require "./put-io"
+require "./put-io/cli/config"
 require "pretty_print"
 
 class PutIO
   class CLI
+    APP_ID = 4722_i64
     enum Commands
       Help
       AccountInfo
       List
       Get
       Path
+      Login
     end
     enum OutputFormat
       Unset
@@ -22,11 +25,12 @@ class PutIO
     @@DEFAULT_PROFILE = "default"
 
     property configfile : Path
-    property dbfile : Path
+    property config : JSON::Any
+    # property dbfile : Path
     property verbose : Bool = false
     property profile : String
     property application_secret : String?
-    property client_id : Int64?
+    property app_id : Int64? = APP_ID
     property token : String
     property command : Commands
     property putio : PutIO
@@ -42,7 +46,7 @@ class PutIO
 
     def initialize(argv : Array(String) = ARGV)
       configfile : Path? = nil
-      dbfile : Path? = nil
+      # dbfile : Path? = nil
       profile : String? = nil
       command : Commands? = nil
 
@@ -51,8 +55,8 @@ class PutIO
         case arg
         when "--configfile", "-c"
           configfile = Path[argv.shift]
-        when "--dbfile", "-D"
-          dbfile = Path[argv.shift]
+          # when "--dbfile", "-D"
+          # dbfile = Path[argv.shift]
         when "--profile", "-p"
           profile = argv.shift
         when "--verbose", "-v"
@@ -111,7 +115,7 @@ class PutIO
                     else
                       raise "no configfile defined"
                     end
-      @dbfile = dbfile = Path[dbfile || ENV["PUTIO_DB"]? || "#{ENV["HOME"]}/Library/Put.IO/putio.sqlite3"]
+      # @dbfile = dbfile = Path[dbfile || ENV["PUTIO_DB"]? || "#{ENV["HOME"]}/Library/Put.IO/putio.sqlite3"]
       @profile = profile = (profile || ENV["PUTIO_PROFILE"]? || @@DEFAULT_PROFILE).to_s
       if @output_format == OutputFormat::Auto
         if @output_file
@@ -125,41 +129,48 @@ class PutIO
 
       if verbose
         STDERR.puts "configfile: #{configfile}"
-        STDERR.puts "dbfile: #{@dbfile}"
+        # STDERR.puts "dbfile: #{@dbfile}"
       end
 
       # read the config file
-      config = File.open(configfile) do |input|
-        JSON.parse(input)
+      begin
+        @config = File.open(configfile, "r") do |input|
+          JSON.parse(input)
+        end
+      rescue
+        @config = JSON::Any::Object.new
+        File.write(configfile, @config.to_json)
       end
-      raise "#{PROGRAM_NAME}: #{configfile}: profile '#{@profile}' not found" unless config[profile]?
-      while config[profile].as_s?
-        new_profile = config[profile].as_s
-        raise "#{PROGRAM_NAME}: #{configfile}: profile '#{new_profile}' not found from 'use' directive in profile '#{profile}'" unless config[new_profile]?
+      config = @config
+      raise "#{PROGRAM_NAME}: #{configfile}: profile '#{@profile}' not found" unless @config[profile]?
+      while @config[profile].as_s?
+        new_profile = @config[profile].as_s
+        raise "#{PROGRAM_NAME}: #{configfile}: profile '#{new_profile}' not found from 'use' directive in profile '#{profile}'" unless @config[new_profile]?
         STDERR.puts "use profile: #{profile} -> #{new_profile}" if verbose
         @profile = profile = new_profile
       end
 
       STDERR.puts "profile: #{@profile}" if verbose
       ["token"].each do |required_key|
-        raise "#{PROGRAM_NAME}: #{@configfile}: profile '#{@profile}': #{required_key} not defined" unless config[profile][required_key]?
+        raise "#{PROGRAM_NAME}: #{@configfile}: profile '#{@profile}': #{required_key} not defined" unless @config[profile][required_key]?
       end
-      @application_secret = config[profile]["application_secret"].as_s if config[profile]["application_secret"]?
-      @client_id = config[profile]["client_id"].as_i64 if config[profile]["client_id"]?
-      @token = config[profile]["token"].as_s
+      @application_secret = @config[profile]["application_secret"].as_s if config[profile]["application_secret"]?
+      @app_id = @config[profile]["client_id"].as_i64 if config[profile]["client_id"]?
+      @app_id = @config[profile]["app_id"].as_i64 if config[profile]["app_id"]?
+      @token = @config[profile]["token"].as_s if config[profile]["token"]?
       if verbose
         STDERR.puts "application_secret: #{@application_secret.inspect}"
         STDERR.puts "client_id: #{@client_id.inspect}"
-        # STDERR.puts "token: #{@token.inspect}"
+        STDERR.puts "token: #{@token.inspect}"
       end
 
-      Dir.mkdir_p @dbfile.parent unless Dir.exists? @dbfile.parent
+      # Dir.mkdir_p @dbfile.parent unless Dir.exists? @dbfile.parent
       @putio = PutIO.new(
-        client_id: @client_id,
+        app_id: @app_id,
         application_secret: @application_secret,
         token: @token,
-        dbfile: @dbfile,
       )
+      # dbfile: @dbfile,
     end
 
     private def prepare_output
@@ -229,6 +240,8 @@ class PutIO
         else
           STDERR.puts "No entries found"
         end
+      when Commands::Login
+        raise "cannot login, token already defined" if @token
       else
         raise "#{command}: unknown command"
       end
